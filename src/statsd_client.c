977bcb4f4f3861068301f3106035f664682263ab
Jeff Buchbinder <jeff@ourexchange.net>
2011-11-07 22:11:04 -0500

    Add performance testing mode for client.

--- src/statsd_client.c
+++ src/statsd_client.c
@@ -16,9 +16,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 uint32_t *resolve_host(const char *addr);
 void usage(char *argv[]);
+
+double get_time () {
+  struct timeval t;
+  struct timezone tzp;
+  gettimeofday(&t, &tzp);
+  return t.tv_sec + t.tv_usec*1e-6;
+}
 
 int main(int argc, char *argv[]) {
   struct sockaddr_in *sa;
@@ -27,10 +35,10 @@ int main(int argc, char *argv[]) {
   char *host = NULL, *counter = NULL, *timer = NULL;
   uint32_t *ip;
   long value;
-  int port = 8125, sample_rate = 1;
+  int port = 8125, sample_rate = 1, performance_test = 0;
 
   int opt;
-  while ((opt = getopt(argc, argv, "hH:p:c:v:t:s:")) != -1) {
+  while ((opt = getopt(argc, argv, "hH:p:c:v:t:s:P")) != -1) {
     switch (opt) {
       case 'h':
         usage(argv);
@@ -57,6 +65,9 @@ int main(int argc, char *argv[]) {
       case 'v':
         value = atol(optarg);
         break;
+      case 'P':
+        performance_test = 1;
+        break;
     }
   }
 
@@ -65,7 +76,6 @@ int main(int argc, char *argv[]) {
     usage(argv);
     return 1;
   }
-
 
   /* Format message */
   if (timer != NULL) {
@@ -82,8 +92,23 @@ int main(int argc, char *argv[]) {
   sa->sin_family = AF_INET;
   sa->sin_port = htons(port);
   memcpy(&sa->sin_addr, &ip, sizeof(ip));
-  if (!sendto(s, (char*) buf, strlen(buf), 0, (struct sockaddr*) sa, sizeof(struct sockaddr_in))) {
-    return 1;
+  if (performance_test) {
+    double starting_time, ending_time;
+    int err = 0;
+    int iter;
+    starting_time = get_time();
+    for (iter=0; iter<10000; iter++) {
+      if (!sendto(s, (char*) buf, strlen(buf), 0, (struct sockaddr*) sa, sizeof(struct sockaddr_in))) {
+        err++;
+      }
+    }
+    ending_time = get_time();
+    printf("10k queries in %f seconds\n", ending_time - starting_time);
+    return 0;
+  } else {
+    if (!sendto(s, (char*) buf, strlen(buf), 0, (struct sockaddr*) sa, sizeof(struct sockaddr_in))) {
+      return 1;
+    }
   }
   return 0;
 }
@@ -111,13 +136,14 @@ uint32_t *resolve_host(const char *addr) {
 }
 
 void usage(char *argv[]) {
-  fprintf(stderr, "Usage: %s [-h] [-H host] [-p port] [-c counter] [-t timer] [-v value]\n", argv[0]);
+  fprintf(stderr, "Usage: %s [-hP] [-H host] [-p port] [-c counter] [-t timer] [-v value]\n", argv[0]);
   fprintf(stderr, "\t-h               This help screen\n");
   fprintf(stderr, "\t-H host          Destination statsd server name/ip\n");
   fprintf(stderr, "\t-p port          Destination statsd server port (defaults to 8125)\n");
   fprintf(stderr, "\t-c counter       Counter name (required, or timer)\n");
   fprintf(stderr, "\t-t timer         Timer name (required, or counter)\n");
   fprintf(stderr, "\t-v value         Value (required)\n");
+  fprintf(stderr, "\t-P               Performance testing mode (disabled by default\n");
   fprintf(stderr, "\nBoth a counter and timer cannot exist at the same time.\n");
 }
 
