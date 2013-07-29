208f9603a75b1f6a1c52ba592613a7130814a793
Jeff Buchbinder <jeffreymbuchbinder@gmail.com>
2013-07-29 12:52:21 -0400

    Updated uthash from upstream git source.

--- src/uthash/utlist.h
+++ src/uthash/utlist.h
@@ -416,6 +416,15 @@ do {                                                                            
 #endif
 /* end VS2008 replacements */
 
+#define LL_COUNT(head,el,counter)                                                              \
+    LL_COUNT2(head,el,counter,next)                                                            \
+
+#define LL_COUNT2(head,el,counter,next)                                                        \
+{                                                                                              \
+    counter = 0;                                                                               \
+    LL_FOREACH2(head,el,next){ ++counter; }                                                    \
+}
+
 #define LL_FOREACH(head,el)                                                                    \
     LL_FOREACH2(head,el,next)
 
@@ -563,6 +572,14 @@ do {                                                                            
   }                                                                                            \
 } while (0) 
 
+#define DL_COUNT(head,el,counter)                                                              \
+    DL_COUNT2(head,el,counter,next)                                                            \
+
+#define DL_COUNT2(head,el,counter,next)                                                        \
+{                                                                                              \
+    counter = 0;                                                                               \
+    DL_FOREACH2(head,el,next){ ++counter; }                                                    \
+}
 
 #define DL_FOREACH(head,el)                                                                    \
     DL_FOREACH2(head,el,next)
@@ -658,6 +675,15 @@ do {                                                                            
      if ((del) == (head)) (head)=(del)->next;                                                  \
   }                                                                                            \
 } while (0) 
+
+#define CDL_COUNT(head,el,counter)                                                             \
+    CDL_COUNT2(head,el,counter,next)                                                           \
+
+#define CDL_COUNT2(head, el, counter,next)                                                     \
+{                                                                                              \
+    counter = 0;                                                                               \
+    CDL_FOREACH2(head,el,next){ ++counter; }                                                   \
+}
 
 #define CDL_FOREACH(head,el)                                                                   \
     CDL_FOREACH2(head,el,next)
